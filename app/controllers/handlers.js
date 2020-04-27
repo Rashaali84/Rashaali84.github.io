bfab71602d2965edf0d588ebce6332e4d05fb49e
Rasha ali <rashaali84@gmail.com>
2020-04-27 23:04:20 +0200

    display personal details

---
+++ app/controllers/handlers.js
@@ -0,0 +1,36 @@
+
+
+import bioModule from '../models/bio.js';
+import viewModule from '../views/view.js';
+//middle tier logic is here // call data tier with needed input
+class handlers {
+    async hanldleBio() {
+        const myBio = new bioModule('rashaali84');
+
+        await myBio.getPersonalDetails(myBio).then(dataFetched => {
+            //populate the fetched data
+            myBio.name = dataFetched.name;
+            myBio.login = dataFetched.login;
+            myBio.avatar_url = dataFetched.avatar_url;
+            myBio.api_Url = dataFetched.url;
+            myBio.followers_Url = dataFetched.followers_url;
+            myBio.bio = dataFetched.bio;
+            myBio.blog = dataFetched.blog;
+            myBio.public_repos_count = dataFetched.public_repos;
+        });
+        //call another api to fetch list of projects 
+        await myBio.getPublicRepotList(myBio).then(dataFetched => {
+            debugger;
+            //fill the list of projects in the myBio object
+            dataFetched.forEach(function (projItem) {
+                myBio.repoList.push(projItem);
+            });
+            //call view functions to display results and pass on bio object
+            console.log(myBio);
+            viewModule.displayBio(myBio);
+            viewModule.displayProjectList(myBio);
+        });
+    }
+}
+new handlers().hanldleBio();
+
---
+++ app/views/view.js
@@ -0,0 +1,33 @@
+export default class view {
+    static displayBio(bioObj) {
+        debugger;
+        const bio = document.getElementById('bio');
+        bio.innerText = bioObj.bio;
+        const bioImg = document.getElementById('bio-img');
+        bioImg.src = bioObj.avatar_url;
+
+    }
+    static displayProjectList(bioObj) {
+        debugger;
+        const projectContainer = document.getElementById('proj-container');
+        let html = '';
+        html += `<h3>My total number of projects on github : ${bioObj.public_repos_count}</h3><br/><br/>`;
+        bioObj.repoList.forEach(element => {
+
+            html += `<div class="col-sm-3">
+            <h6>${element.name}</h>
+            <a href=${element.html_url === null ? 'https://github.com/Rashaali84' : element.html_url}>
+            <img
+                src="assets/repo-icon.jpg"
+                class="img-responsive"
+                style="width: 100%;"
+                alt=${element.description === null ? 'no description found' : element.description}
+            />
+            </a>
+        </div>`
+        });
+        projectContainer.innerHTML = html;
+
+
+    }
+}
